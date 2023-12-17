b887013c304beb4c7a6954b2faeec7f71828fa1e
Rami Sabbagh <rami.sab07@gmail.com>
2023-12-18 01:06:47 +0300

    Try out streamlit

--- src/uni_cv_project/__main__.py
+++ src/uni_cv_project/__main__.py
@@ -1,3 +1,34 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
 
-if __name__ == '__main__':
-    print("Hello World from __main__.py!")
+"""
+# Puzzle Solver
+
+A university project built using [OpenCV], [Python] and [Streamlit]!
+
+[OpenCV]: https://opencv.org/
+[Python]: https://python.org/
+[Streamlit]: https://streamlit.io/
+"""
+
+x = st.slider('x')
+
+f'{x} squared is {x ** 2}!'
+
+
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
