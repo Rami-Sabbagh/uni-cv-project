38fcde4de3004edb9b2d5b3303964cc422bf51f4
Rami Sabbagh <rami.sab07@gmail.com>
2023-12-21 10:33:57 +0300

    matcher gui draft

---
+++ src/uni_cv_project/pages/04_Grid_Matcher.py
@@ -0,0 +1,111 @@
+import os
+
+import cv2 as cv
+import numpy as np
+import pandas as pd
+import altair as alt
+import streamlit as st
+
+from time import perf_counter
+from numpy.random import default_rng
+
+from utils.grid import Grid, GridIdentifier
+from utils.image import Image
+
+from typing import *
+from numpy.typing import NDArray
+
+st.set_page_config('Grid Matcher', '🔣')
+rng = default_rng()
+
+
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+"""
+# 🔣 Grid Matcher
+
+This tool reorders a grid of rectangular pieces to match a reference image
+as closest as possible.
+
+To get started please select the target image you wish.
+"""
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+
+@st.cache_resource(show_spinner=False)
+def load_image(file, default = 'uni-1.jpg') -> tuple[str, NDArray]:
+    if file is None:
+        return default, cv.imread('images/' + default)
+    else:
+        return file.name, Image.decode(file.getvalue())
+
+image_name, image = load_image(st.file_uploader('Target Image', ['png', 'jpg', 'gif', 'bmp', 'webp']))
+# st.image(image, f'Target Image: "{image_name}"', channels='BGR')
+
+hint_file = st.file_uploader('Hint Image', ['png', 'jpg', 'gif', 'bmp', 'webp'])
+if hint_file is None: hint_name, hint = image_name, image
+else: hint_name, hint = load_image(hint_file)
+
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+'## Grid Shape'
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+
+col1, col2 = st.columns([1, 3])
+if col2.toggle('Automatic Detection', False):
+    std_threshold = col1.number_input('$\sigma$ Threshold', 1, value=4)
+    widest_gap = col1.number_input('Widest Gap', 10, value=50)
+
+    with st.spinner('Detecting shape...'):
+        rows, columns = GridIdentifier.identify_grid_shape(
+            image, std_threshold=std_threshold, widest_gap=widest_gap,
+        )
+
+else:
+    rows = col1.number_input('Rows', 1, None, 5)
+    columns = col1.number_input('Columns', 1, None, 5)
+
+
+col2.write(pd.DataFrame({
+    'Rows': {
+        'Grid': rows,
+        'Cell': image.shape[0] // rows,
+    },
+    'Columns': {
+        'Grid': columns,
+        'Cell': image.shape[1] // columns,
+    },
+}))
+
+shuffle_enabled = st.toggle('Shuffle Cells', True)
+
+if shuffle_enabled:
+    col1, col2 = st.columns([3, 1])
+    seed = col1.number_input(
+        'Seed', label_visibility='collapsed',
+        value=rng.integers(1e9, 1e10) if col2.button('♻️ Randomize') else 948135, 
+    )
+
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+'### Input Preview'
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+
+
+with st.spinner('Preparing Grid...'):
+    cells = Grid.split(image, rows, columns)[0]
+    if shuffle_enabled: cells = Grid.shuffle(cells, seed)
+
+col1, col2 = st.columns(2)
+
+col1.image(Grid.merge(cells), f'Target Grid: "{image_name}"', channels='BGR')
+col2.image(hint, f'Hint Image: "{hint_name}"', channels='BGR')
+
+
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+'## Matching Results'
+#==---==---==---==---==---==---==---==---==---==---==---==---==---==---==---==#
+
+def image_distance(a: NDArray, b: NDArray) -> float:
+    return np.average(np.sum(np.square(a - b), axis=-1))
+
+# with st.spinner('Matching...'):
+
+
+
